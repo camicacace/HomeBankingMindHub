5e0e62e23b0d507069059bee34555a792fbbfbd8
Camila Cacace <102537508+camicacace@users.noreply.github.com>
2024-05-27 16:32:40 -0300

    configuracion inicial con clientes

---
+++ Models/DBInitializer.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context) {
+
+            var clients = new Client[]
+            {
+                new Client{ FirstName = "Alejandro", LastName = "Gomez", Email = "alegomez@gmail.com", Password = "1234"},
+                new Client{ FirstName = "Juana", LastName = "Lopez", Email = "jlopez@gmail.com", Password = "5678"}
+            };
+
+            context.Clients.AddRange(clients);
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
+using HomeBankingMindHub.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<HomeBankingContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConnection")));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    //todos los servicios que hay en la app
+    var services = scope.ServiceProvider;
+    try
+    {
+        //recuperamos el servicio de contexto de HomeBankingContext
+        var context = services.GetRequiredService<HomeBankingContext>();
+        DBInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ha ocurrido un error al enviar la información a la base de datos");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
