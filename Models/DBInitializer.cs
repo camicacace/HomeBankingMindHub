03cc490068ca971c62fe02d269a21801d58ad78d
Camila Cacace <102537508+camicacace@users.noreply.github.com>
2024-05-28 09:39:51 -0300

    mas clientes

--- Models/DBInitializer.cs
+++ Models/DBInitializer.cs
@@ -4,14 +4,19 @@
     {
         public static void Initialize(HomeBankingContext context) {
 
-            var clients = new Client[]
-            {
+            if (!context.Clients.Any())
+           {
+                var clients = new Client[]
+                {
                 new Client{ FirstName = "Alejandro", LastName = "Gomez", Email = "alegomez@gmail.com", Password = "1234"},
-                new Client{ FirstName = "Juana", LastName = "Lopez", Email = "jlopez@gmail.com", Password = "5678"}
-            };
+                new Client{ FirstName = "Juana", LastName = "Lopez", Email = "jlopez@gmail.com", Password = "5678"},
+                new Client { FirstName = "Maria", LastName = "Rodriguez", Email = "mr@gmail.com", Password = "9012"},
+                new Client { FirstName = "Lucas", LastName = "Rodriguez", Email = "lucasr@gmail.com", Password = "3456"},
+                };
 
-            context.Clients.AddRange(clients);
-            context.SaveChanges();
+                context.Clients.AddRange(clients);
+                context.SaveChanges();
+            }
         }
     }
 }
