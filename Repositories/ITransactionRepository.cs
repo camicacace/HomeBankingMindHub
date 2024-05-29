bfc4e0f8f09764a97f8b76c31deca22b506169c7
Camila Cacace <102537508+camicacace@users.noreply.github.com>
2024-05-29 19:02:21 -0300

    agrego TransactionRepository

---
+++ Repositories/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public interface ITransactionRepository
+    {
+        IEnumerable<Transaction> GetAllTransactions();
+        void Save(Transaction transaction);
+        Transaction FindById(long id);
+    }
+}
---
+++ Repositories/Implementations/TransactionRepository.cs
@@ -0,0 +1,26 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories.Implementations
+{
+    public class TransactionRepository : RepositoryBase<Transaction> , ITransactionRepository
+    {
+        public TransactionRepository(HomeBankingContext repositoryContext) : base(repositoryContext) { }
+
+        public Transaction FindById(long id)
+        {
+            return FindByCondition(transaction => transaction.Id == id)
+                .FirstOrDefault();
+        }
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            return FindAll()
+                .ToList();
+        }
+
+        public void Save(Transaction transaction)
+        {
+            Create(transaction);
+            SaveChanges();
+        }
+    }
+}
