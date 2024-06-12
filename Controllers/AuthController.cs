9f90efa4653ba989b47d7562d5c1158e435e0424
Camila Cacace <102537508+camicacace@users.noreply.github.com>
2024-06-12 07:45:47 -0300

    Modificaciones de token

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -61,7 +61,7 @@ namespace HomeBankingMindHub.Controllers
                 }
 
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
                 var token = new JwtSecurityToken(
                     claims: claims,
